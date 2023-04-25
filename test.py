338565c2272c433c823ba14f6a44679961258d6a
Young Oh <dureng5@gmail.com>
2023-04-25 14:30:24 +0900

    실패하는 테스트를 작성한다.

--- test.py
+++ test.py
@@ -53,6 +53,9 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(Money.franc(10), five.times(2))
         self.assertEqual(Money.franc(15), five.times(3))
 
+    def test_currency(self):
+        self.assertEqual("USD", Money.dollar(1).currency())
+        self.assertEqual("CHF", Money.franc(1).currency())
 
 if __name__ == '__main__':
     unittest.main()

d60ca0b81431b0f007ca4dc8127e4aac27633c9c
Young Oh <dureng5@gmail.com>
2023-04-25 10:35:01 +0900

    1~2. 재빨리 작은 테스트를 하나 추가한다. 테스트를 실행해서 테스트가 실패하는 것을 확인한다.

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestMethods(unittest.TestCase):
+
+    def test_multiplication(self):
+        five = Dollar(5)
+        five.times(2)
+        self.assertEqual(10, five.amount)
+
+
+if __name__ == '__main__':
+    unittest.main()

82b6cd361d2ff18d3d6ae52d8ad2b0967c1dec3c
Young Oh <dureng5@gmail.com>
2023-04-25 14:30:09 +0900

    리팩토링

--- test.py
+++ test.py
@@ -8,6 +8,17 @@ class Money:
 
     def __eq__(self, money):
         return self._amount == money._amount and self.__class__ == money.__class__
+    
+    def times(self, multiplier):
+        pass
+
+    @classmethod
+    def dollar(cls, amount):
+        return Dollar(amount)
+    
+    @classmethod
+    def franc(cls, amount):
+        return Franc(amount)
 
 class Dollar(Money):
     def times(self, multiplier):
@@ -24,23 +35,23 @@ class TestMethods(unittest.TestCase):
 
     # 어떤 금액(주가)을 어떤 수(주식의 수)에 곱한 금액을 결과로 얻을 수 있어야 한다
     def test_multiplication(self):
-        five = Dollar(5)
+        five = Money.dollar(5)
         
-        self.assertEqual(Dollar(10), five.times(2))
-        self.assertEqual(Dollar(15), five.times(3))
+        self.assertEqual(Money.dollar(10), five.times(2))
+        self.assertEqual(Money.dollar(15), five.times(3))
 
     def test_equality(self):
-        self.assertTrue(Dollar(5).__eq__(Dollar(5)))
-        self.assertFalse(Dollar(5).__eq__(Dollar(6)))
-        self.assertTrue(Franc(5).__eq__(Franc(5)))
-        self.assertFalse(Franc(5).__eq__(Franc(6)))
-        self.assertFalse(Franc(5).__eq__(Dollar(5)))
+        self.assertTrue(Money.dollar(5).__eq__(Money.dollar(5)))
+        self.assertFalse(Money.dollar(5).__eq__(Money.dollar(6)))
+        self.assertTrue(Money.franc(5).__eq__(Money.franc(5)))
+        self.assertFalse(Money.franc(5).__eq__(Money.franc(6)))
+        self.assertFalse(Money.franc(5).__eq__(Money.dollar(5)))
 
     def test_franc_multiplication(self):
-        five = Franc(5)
+        five = Money.franc(5)
         
-        self.assertEqual(Franc(10), five.times(2))
-        self.assertEqual(Franc(15), five.times(3))
+        self.assertEqual(Money.franc(10), five.times(2))
+        self.assertEqual(Money.franc(15), five.times(3))
 
 
 if __name__ == '__main__':

40b2a1eccaddbb5888230627d854eb14b78d6774
Young Oh <dureng5@gmail.com>
2023-04-25 10:49:34 +0900

    5. 중복을 제거하기 위해 리팩토링을 한다.

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 import unittest
 
 class Dollar:
-    amount = 10
+    amount = 0
 
     def __init__(self, amount):
-        pass
+        self.amount = amount
 
     def times(self, multiplier):
-        pass
+        self.amount *= multiplier
 
 
 class TestMethods(unittest.TestCase):
 
+    # 어떤 금액(주가)을 어떤 수(주식의 수)에 곱한 금액을 결과로 얻을 수 있어야 한다
     def test_multiplication(self):
         five = Dollar(5)
         five.times(2)
